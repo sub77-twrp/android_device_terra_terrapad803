5d3c49a27a97296375f3cfe36cf31149954e9814
sub77 <sub77@ymail.com>
2016-08-30 10:53:44 +0200

    Update BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,8 +15,8 @@ TARGET_CPU_MEMCPY_OPT_DISABLE := true
 TARGET_BOOTLOADER_BOARD_NAME := mt6582
 
 # Partitions & Image
-BOARD_BOOTIMAGE_PARTITION_SIZE := 13485760
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 13485760
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1468006400
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5452595200
 BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
@@ -30,7 +30,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 TARGET_PREBUILT_KERNEL := device/terra/terrapad803/kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/terra/terrapad803/MTKbootimg.mk
 BOARD_CUSTOM_BOOTIMG := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --board P301_KK_V0.06
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 
 # Healthd & Charging
 HEALTHD_ENABLE_TRICOLOR_LED := true
@@ -47,19 +47,19 @@ BOARD_HEALTHD_CUSTOM_CHARGER_RES := device/terra/terrapad803/charger/images
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := device/terra/terrapad803/recovery.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+#TARGET_RECOVERY_FSTAB := device/terra/terrapad803/recovery/etc/twrp.fstab
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+DEVICE_RESOLUTION := 800x1280
 
 # Sepolicy hack for old kernel, our mt6582 & mt6592 version is 26.
 POLICYVERS := 26
 
 # TWRP
-TW_DEVICE_SPECIFIC_VERSION := 3.0.2-3
+TW_DEVICE_SPECIFIC_VERSION := 3.0.2-15
 TW_THEME := portrait_hdpi
-DEVICE_RESOLUTION := 800x1280
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 800
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_NO_SCREEN_BLANK := true
@@ -68,11 +68,12 @@ TW_INTERNAL_STORAGE_PATH := "/emmc"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+#TW_INCLUDE_CRYPTO := true
 #TW_CRYPTO_FS_TYPE := "ext4"
 #TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p7"
 #TW_CRYPTO_MNT_POINT := "/and-sec"
 #TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
-#TW_EXCLUDE_SUPERSU := true
+#TW_NO_USB_STORAGE := true
 TW_INCLUDE_FB2PNG := true
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
-
+#TW_EXCLUDE_SUPERSU := true
